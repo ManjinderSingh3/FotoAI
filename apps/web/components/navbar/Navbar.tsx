2f5267254bf64b7934b0514e4610e9ef362a1d8b
Manjinder <msinghw3@gmail.com>
2025-06-03 17:37:44 -0500

    Dark and Light theme functionality added

--- apps/web/components/navbar/Navbar.tsx
+++ apps/web/components/navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export function Navbar() {
   return (
@@ -21,7 +22,7 @@ export function Navbar() {
               href="/"
               className="flex items-center gap-2 transition-opacity hover:opacity-90"
             >
-              <LogoIcon className="h-8 w-8 text-black" />
+              <LogoIcon className="h-8 w-8 text-black dark:text-white" />
               <span className="hidden font-bold font-mono text-xl sm:inline-block mt-1.5">
                 Foto<span className="text-cyan-500">AI</span>
               </span>
@@ -32,23 +33,26 @@ export function Navbar() {
         {/* Pricing and Authentication */}
         <div>
           <SignedIn>
+            <ThemeToggle />
             <UserButton />
           </SignedIn>
           <SignedOut>
             <div className="flex space-x-4">
+              <ThemeToggle />
               <button className="group relative rounded-full p-px text-sm/6 text-zinc-900 hover:text-zinc-400 dark:text-zinc-400 duration-300 dark:hover:text-zinc-100 border hover:shadow-[0_0_10px_rgba(56,189,248,0.6)]">
                 <div className="relative z-10 rounded-full  px-4 py-1 ring-1 ring-white/10">
                   Pricing
                 </div>
                 <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-400/0 via-cyan-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
               </button>
-
-              <button className="group relative rounded-full p-px text-sm/6 text-zinc-400 duration-300 hover:text-zinc-100 hover:shadow-[0_0_10px_rgba(56,189,248,0.6)]">
-                <div className="relative z-10 rounded-full bg-zinc-950 px-4 py-1 ring-1 ring-white/10">
-                  Sign In
-                </div>
-                <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-400/0 via-cyan-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
-              </button>
+              <SignInButton>
+                <button className="group relative rounded-full p-px text-sm/6 text-zinc-400 duration-300 hover:text-zinc-100 hover:shadow-[0_0_10px_rgba(56,189,248,0.6)]">
+                  <div className="relative z-10 rounded-full bg-zinc-950 px-4 py-1 ring-1 ring-white/10">
+                    Sign In
+                  </div>
+                  <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-cyan-400/0 via-cyan-400/90 to-cyan-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+                </button>
+              </SignInButton>
             </div>
           </SignedOut>
         </div>
