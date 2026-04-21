771ec549c0909d5a65d8df03703ba15dee62583e
Manjinder <msinghw3@gmail.com>
2026-04-20 23:27:25 -0500

    Boilerplate code fro auth and payment modules

--- apps/backend-spring/fotoai-auth/src/test/java/com/fotoai/auth/AuthApplicationTests.java
+++ apps/backend-spring/fotoai-auth/src/test/java/com/fotoai/auth/AuthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fotoai.backend_spring;
+package com.fotoai.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendSpringApplicationTests {
+class AuthApplicationTests {
 
 	@Test
 	void contextLoads() {
